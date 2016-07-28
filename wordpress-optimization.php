bc521d59507416aed3583ddbca0197da8d470f8e
Mohamed Safouan Besrour <besrour_2010@live.com>
2016-07-28 16:13:42 +0100

    Update wordpress-optimization.php

--- wordpress-optimization.php
+++ wordpress-optimization.php
@@ -20,6 +20,7 @@ function whtop_setup()
     remove_action( 'wp_head', 'rest_output_link_wp_head', 10 );
     remove_action( 'wp_head', 'wp_oembed_add_discovery_links', 10 );
     remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
+    remove_action( 'wp_head', 'wp_oembed_add_host_js' );
    
     remove_action( 'rss2_head', 'the_generator' );
     remove_action( 'rss_head',  'the_generator' );
@@ -31,6 +32,13 @@ function whtop_setup()
     remove_action( 'comments_atom_head', 'the_generator' );
     remove_action( 'admin_print_scripts', 'print_emoji_detection_script' );
     remove_action( 'wp_print_styles', 'print_emoji_styles' );
+    remove_action( 'template_redirect', 'rest_output_link_header', 11, 0 );
+    // Disable REST API v1
+    add_filter('json_enabled','__return_false');
+    add_filter('json_jsonp_enabled','__return_false');
+    // Disable REST API v2
+    add_filter('rest_enabled','__return_false');
+    add_filter('rest_jsonp_enabled','__return_false');
 }
 add_action( 'after_setup_theme', 'whtop_setup' );
 
@@ -42,7 +50,3 @@ function remove_cssjs_ver( $src ) {
 }
 add_filter( 'style_loader_src', 'remove_cssjs_ver', 10, 2 );
 add_filter( 'script_loader_src', 'remove_cssjs_ver', 10, 2 );
-
-//Remove JSON API
-add_filter('json_enabled', '__return_false');
-add_filter('json_jsonp_enabled', '__return_false');
